ba70d9d77073309c1c672fc85ac611264d1e2f97
Lennart Van der Goten <lvandergoten@gmail.com>
2020-10-13 22:22:43 +0200

    Added R Scripts

--- visualize_elapsed_times.R
+++ visualize_elapsed_times.R
@@ -2,18 +2,26 @@ library(readr)
 library(dplyr)
 library(stringr)
 library(ggplot2)
+library(tidyr)
 library(rstudioapi)
 setwd(dirname(getActiveDocumentContext()$path))
 
 data <- read_csv("elapsed_times.csv") %>%
   mutate(PROGRAM=str_split_fixed(as.character(PROGRAM), pattern="", n=2),
          PROGRAM=str_c(PROGRAM[,1], ".", PROGRAM[,2]),
-         PROGRAM=as.factor(PROGRAM))
+         PROGRAM=as.factor(PROGRAM)) %>%
+  select(-RESULT)
+
+data <- data %>%
+  pivot_longer(c(TIME_SAMPLING, TIME_DIST), names_to = "MODE", values_to = "TIME") %>%
+  group_by(PROGRAM, NPROC, MODE) %>%
+  summarize(TIME = min(TIME))
 
 data %>%
   ggplot(aes(x=NPROC, y=TIME, color=PROGRAM, linetype=PROGRAM, shape=PROGRAM)) +
   geom_point() +
   geom_line() +
+  facet_wrap(~MODE, scales = "free") +
   xlab("#Processes") +
   ylab("Time [sec]") +
-  ggtitle("Wall-clock time for each subtask [averaged over 10 runs]")
+  ggtitle("Wall-clock time for each subtask [minimum over 2 runs]")
